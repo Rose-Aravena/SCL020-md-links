44ac98817fe655503be26a260daf3d385517b98a
Rose Aravena Madariaga <rosearavenamadariaga@MacBook-Air-de-Rose.local>
2022-07-05 19:26:54 -0400

    i dependencies and folder dummy

---
+++ dummy/README.md
@@ -0,0 +1,3 @@
+archivo de prueba
+
+mi perfil github: https://github.com/Rose-Aravena
--- md-links.js
+++ md-links.js
@@ -1,3 +1,22 @@
 
 const axios = require('axios').default;
 const chalk = require('chalk');
+const fs = require('fs');
+const path = require('path')
+const marked = require('marked');
+const cheerio = require('cheerio');
+
+// const files = fs.readdirSync('./') // Reads the contents of the directory. Returns: <string[]>
+const arch = './dummy/README.md'
+// fs.readFile(arch, 'utf8', (error, data) => {
+//     if(error) return console.error(error);
+//     console.log(data);
+// })
+
+const read = fs.readFileSync(arch, 'utf8')
+const html = marked.parse(read)
+const $ = cheerio.load(html)
+const resul = $('p a') // indicamos lo que queremos extraer del html
+console.log(html)
+console.log(resul.attr('href'))
+console.log(resul.text())
